ad5c6ecde53e242b7fb044a33cfd7269440ebe9d
mikoto <mikoto@ungine.cn>
2023-11-16 12:48:18 +0800

    init

---
+++ src/main/java/net/mikoto/aozora/AozoraApplication.java
@@ -0,0 +1,13 @@
+package net.mikoto.aozora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AozoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AozoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/mikoto/aozora/AozoraApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mikoto.aozora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AozoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
